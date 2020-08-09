ea670ca4643be61d43abf196431d78c2d4a41d18
Eddie <eddievictor123@gmail.com>
2020-08-09 16:31:33 -0300

    testando if,else, e %

---
+++ testando.js
@@ -0,0 +1,18 @@
+/*let arroz = "Arroz: R$: 15,00";
+
+
+console.log(arroz);
+*/
+
+var NumeroFixado=198789451
+let RestoDaDivisaoDoNumeroFixadoPor2;
+RestoDaDivisaoDoNumeroFixadoPor2 = NumeroFixado%2;
+console.log(RestoDaDivisaoDoNumeroFixadoPor2);
+
+    if(RestoDaDivisaoDoNumeroFixadoPor2==1){
+        console.log("Número informado é impar!");
+    }
+       else 
+        {
+            console.log("Numero informado é par");
+        }
